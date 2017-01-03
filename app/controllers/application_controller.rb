503ca47c8a3a328de22004076f3882fb6bfcb3c0
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-03 17:07:11 +0800

    add authenticate to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  check_authorization if: :admin_subdomain?
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:warning] = exception.message
     redirect_to root_path
@@ -10,6 +12,10 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  def admin_subdomain?
+    request.subdomain == "admin"
+  end
 
   def current_user
     User.new(session[:id])
