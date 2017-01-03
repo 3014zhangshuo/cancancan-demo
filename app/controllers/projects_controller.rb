04ae7c221307f3bc535ff0daa073f5447743ad71
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-03 16:25:38 +0800

    add authenticate limit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    authorize! :index, @project
+    #prevents the user from accessing the projects page directly
   end
 end
