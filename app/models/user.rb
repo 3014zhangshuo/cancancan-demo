7713c7d697e45f09c96eedd086a9c41d241cb4b4
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-03 16:15:06 +0800

    add project MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   ROLES = {0 => :guest, 1 => :user, 2 => :moderator, 3 => :admin}
 
-  attr_reader: role
+  attr_reader :role
 
   def initialize(role_id = 0)
     @role = ROLES.has_key?(role_id.to_i) ? ROLES[role_id.to_i] : ROLES[0]
