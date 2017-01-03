28c581f7e3682fcb254b76ad19ed34e9a1673979
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-03 16:58:33 +0800

    add new columnto project is ongoing

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103081249) do
+ActiveRecord::Schema.define(version: 20170103085503) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "ongoing",    default: true
+    t.index ["ongoing"], name: "index_projects_on_ongoing"
   end
 
   create_table "users", force: :cascade do |t|
